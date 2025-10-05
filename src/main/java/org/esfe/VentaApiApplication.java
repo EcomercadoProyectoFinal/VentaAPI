1b226b2875df8a2451d42f7f5afa83c5fb0aa39e
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-10-05 15:32:17 -0600

    se inicio el proyecto para la api de ventas de ecomercado

---
+++ src/main/java/org/esfe/VentaApiApplication.java
@@ -0,0 +1,13 @@
+package org.esfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentaApiApplication.class, args);
+	}
+
+}

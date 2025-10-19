f9d81a61a1cba42da083ae4f367d3825dab99829
Brayan Eduardo Tepas Linares <tl25001@esfe.agape.edu.sv>
2025-10-18 20:21:33 -0600

    mapeo del nombre del metodo de pago en la venta

--- src/main/java/org/esfe/config/ModelMapperConfig.java
+++ src/main/java/org/esfe/config/ModelMapperConfig.java
@@ -1,5 +1,7 @@
 package org.esfe.config;
 
+import org.esfe.dtos.venta.VentaSalida;
+import org.esfe.modelos.Venta;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +10,14 @@ import org.springframework.context.annotation.Configuration;
 public class ModelMapperConfig {
     @Bean
     public ModelMapper modelMapper(){
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+
+        // Configuración para mapear el nombre del MétodoPago en el DTO de VentaSalida
+        modelMapper.createTypeMap(Venta.class, VentaSalida.class)
+            // Origen: Venta -> MetodoPago -> MetodoPago (nombre del método)
+            // Destino: VentaSalida -> setNombreMetodoPago()
+            .addMapping(src -> src.getMetodoPago().getMetodoPago(), VentaSalida::setNombreMetodoPago);
+
+        return modelMapper;
     }
 }
